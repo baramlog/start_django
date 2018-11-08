d859ac25797089cf6ba1ae1bf63922e8fc8b8548
baramlog <baramlog@gmail.com>
2018-11-08 22:39:58 +0900

    start

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from blog.models import Blog, Post
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('title', 'url')
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'modify_date')
+	list_filter = ('modify_date',)
+	search_fields = ('title', 'content')
+	prepopulated_fields = {'slug': ('title',)}
+		
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Post, PostAdmin)
